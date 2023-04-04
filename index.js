c4664d701d7d45e2fe2becbd07e83cae3dd314dd
Greta Bajorek <bajorekgreta1605@gmail.com>
2023-04-04 10:55:55 +0200

    change in index.js

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const convertCurrency = () => {
 	fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${select.value}`)
 		.then(response => response.json())
 		.then(data => {
-			if (isNaN(input.value)) {
-				const message = document.querySelector('h2')
-				message.style.display = 'block'
-				return
-			} else {
-				const conversion = (data.rates[0].mid * Number(input.value)).toFixed(2)
-				output.innerHTML = "to <span style='font-weight: bold;'>" + conversion + '</span> PLN'
-			}
+			const conversion = (data.rates[0].mid * Number(input.value)).toFixed(2)
+			output.innerHTML = `to <span style='font-weight: bold;'>${conversion}</span> PLN`
 		})
+		.catch(e => console.error(e))
 }
 button.addEventListener('click', () => {
+	if (input.value === 'e' || input.value <= 0) {
+		const message = document.querySelector('h2')
+		message.style.display = 'block'
+		return
+	}
 	convertCurrency()
 })
